51960199303aeb358593eeee7aec1720c8e7bc3f
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-05-04 17:01:02 +0300

    context is working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Board from'./components/Board';
 import KeyBoard from './components/KeyBoard';
+import { useState,createContext } from 'react';
+import { boardDefaultValue } from './components/Words';
+
+export const AppContext = createContext()
 
 function App() {
+  const [board, setBoard] = useState(boardDefaultValue);
+
   return (
     <div className="App">
       <nav>
         <h1>WORDLE</h1>
       </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
       <Board />
       <KeyBoard />
-
+      </AppContext.Provider>
      </div>
   );
 }
