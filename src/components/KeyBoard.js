7dbf4bc620a39a26afcbb3bc81640349f3312238
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-05-04 17:37:12 +0300

    change the react script
    in json folder to refresh auto page

--- src/components/KeyBoard.js
+++ src/components/KeyBoard.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function KeyBoard() {
+    const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']; 
+    const keys2 = ['A', 'B', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+    const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
   return (
-    <div>KeyBoard</div>
+      <div className="keyboard">
+          <div className="line1">{keys1.map((key) => {
+              return <div> {key} </div>;
+          }) }</div>
+          <div className="line2">{keys2.map((key) => {
+              return <div> {key} </div>;
+          })}</div>
+          <div className="line3">{keys3.map((key) => {
+              return <div> {key} </div>;
+          })}</div>
+    </div>
   )
 }
 
