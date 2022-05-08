27185af7a60da8670a9296c3df6d55375d1a3dfc
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-05-08 17:42:54 +0300

    refac. key component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,35 @@ function App() {
   const [board, setBoard] = useState(boardDefaultValue);
   const [currentAttempt, setCurrentAttempt] = useState({ attempt: 0, letterPosition: 0 });
 
+
+  const onEnter = () => {
+    if (currentAttempt.letterPosition !== 5) return;
+
+    
+  }
+
+  const onDelete = () => {
+    if (currentAttempt.letterPosition === 0) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = '';
+    setBoard(newBoard);
+    setCurrentAttempt({ ...currentAttempt, letterPosition: currentAttempt.letterPosition - 1 });
+  }
+
+  const onSelectLetter = (key) => {
+    if (currentAttempt.letterPosition > 4) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = key;
+    setBoard(newBoard);
+    setCurrentAttempt({ ...currentAttempt, letterPosition: currentAttempt.letterPosition + 1 });
+  }
+
   return (
     <div className="App">
       <nav>
         <h1>WORDLE</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currentAttempt, setCurrentAttempt }}>
+      <AppContext.Provider value={{ board, setBoard, currentAttempt, setCurrentAttempt, onEnter, onDelete, onSelectLetter }}>
         <div className="game">
           <Board />
           <KeyBoard />
